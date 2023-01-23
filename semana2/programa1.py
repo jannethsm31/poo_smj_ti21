6c3281756ff964ef7ad4bff69e1c3a6c3ed77318
jannethsm31 <122809612+jannethsm31@users.noreply.github.com>
2023-01-23 17:00:21 +0000

    CREAMOS la carpeta semana2 y el programa1.py

---
+++ semana2/programa1.py
@@ -0,0 +1,15 @@
+"""
+    Programa1
+    Nombre:Janneth Santos Molina
+    Fecha:23/01/2023
+    Descripción:Comentarios multilínea, 
+    comentarios de una línea y 
+    concatenación de cadenas, cadena de 
+    caractéres.
+"""
+
+variable1 = "Hola"  # Variable que alamacena una cadena de carácteres
+
+variable2 = " Python"  # Variable que alamcena una cadena de carácteres
+
+print( variable1 + variable2 ) #Concatena 2 cadenas
