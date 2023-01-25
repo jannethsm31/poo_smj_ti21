dfd0ca58fe6981988942642c7e0b9f0d115de0bf
jannethsm31 <122809612+jannethsm31@users.noreply.github.com>
2023-01-25 00:30:07 +0000

    REORGANIZE

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -14,10 +14,10 @@ numero2 = 5
 
 #print("{} + {}".format(numero1, numero2))# manda como resultado la concatenacion como texto las variables
 
-print("{} + {} =".format(numero1, numero2),(numero1 + numero2)) # resultado debe ser "10 + 5= 15"
+#print("{} + {} =".format(numero1, numero2),(numero1 + numero2)) # resultado debe ser "10 + 5= 15"
 
-#print("{} * {} ".format(numero2 , numero1),(numero1 * numero2)) # el resultado debe ser "50=5*10"
+#print((numero1 * numero2)," = {} * {} " .format (numero1 , numero2)) # el resultado debe ser "50=5*10"
 
-#print(" {} / {} =".format(numero1 , numero2),(numero1 / numero2)) # "el resultado de dividir 10/5 = 2"
+#print("El resultado de dividir {} / {} =".format(numero1 , numero2),(numero1 / numero2)) # "el resultado de dividir 10/5 = 2"
 
 
