de21896ecb75efee563178d6e53e311c886c8b99
jannethsm31 <122809612+jannethsm31@users.noreply.github.com>
2023-01-26 19:51:08 +0000

    USAMOS el .format en diferentes circunsitancias

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en python|
 |2.|programa1.py|Hola python concatenado|
-|3.|programa2.py|Concatenación con formato srt
-|4.|programa3.py|Operaciones aritméticas/ print
+|3.|programa2.py|Concatenación con formato srt|
+|4.|programa3.py|Operaciones aritméticas/ print|
+|5.|programa4.py|.format|
---
+++ semana2/programa4.py
@@ -0,0 +1,19 @@
+"""
+    Programa4
+    Nombre:Janneth Santos Molina
+    Fecha:26/01/2023
+    Descripción: Format en diferentes situaciones
+"""
+
+numero1=10
+numero2=5
+
+#print("{} + {} = {} " .format(numero1, numero2, numero1 + numero2))
+
+#print("{n2} + {n2} = {n2}" .format(n1=numero1, n2=numero2, suma= numero1 + numero2))
+
+#print("{n1} + {n2} = {suma}" .format(n1=numero1, n2=numero2, suma= numero1 + numero2)) 
+
+#print("{numero1} + {numero2} = {suma}" .format(numero1=numero1, numero2=numero2, suma= numero1 + numero2))
+
+#print("{numero1} + {numero2} = {}" .format(numero1, numero2, numero1 + numero2))
